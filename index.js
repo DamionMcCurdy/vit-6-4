be8d3dbf9023009f07a7ff54f383cdefd6848009
Damion McCurdy <damion.mccurdy@gmail.com>
2020-06-04 20:37:44 -0600

    n

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-console.log('are we linked up?')
+//document.addEventListenerDOMContentLoaded
+
+//console.log('are we linked up?')
 
 //select body of html with JS
 
@@ -6,5 +8,20 @@ console.log('are we linked up?')
 //insert text into paragraph tag with JS
 //append paragraph element to selected body tag
 
-let container = document.querySelector('#container')
-let message = document.createElement('p')
+//let container = document.querySelector('#container')
+//let message = document.createElement('p')
+
+//message.innerHTML = "Hello sweet world"
+//container.appendChild(message)
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('are we linked up?');
+    // [x] select container div of HTML with JavaScript
+    // [x] create HTML paragraph tag "" ""
+    // [x] insert text into paragraph tag "" ""
+    // [] append paragraph element to selected container
+    let container = document.querySelector('#container')
+    let message = document.createElement('p')
+    message.innerHTML = "Hello Sweet World"
+    container.appendChild(message)
+  })
