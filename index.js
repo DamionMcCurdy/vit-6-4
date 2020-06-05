a5373d6d141020f7355ecbc6b16b05fd87c874a0
Damion McCurdy <damion.mccurdy@gmail.com>
2020-06-04 20:29:08 -0600

    n

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-let first = "Damion"
-let last = "McCurdy"
-console.log('Hello, ' + first + ' ' + last + ', you are home.')
+console.log('are we linked up?')
+
+//select body of html with JS
+
+//create html paragraph tag
+//insert text into paragraph tag with JS
+//append paragraph element to selected body tag
+
+let container = document.querySelector('#container')
+let message = document.createElement('p')
